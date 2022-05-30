6fe1385ae29166f4e3d8a594a76f4ff044d5a1dd
Kailas <kailaskr05@gmail.com>
2022-05-30 15:27:09 +0530

    minor changes

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -6,7 +6,6 @@ import type { LinkOption } from './types';
 
 import { expandEndpoints } from './util';
 
-// var configs = require("../../../config/config.js");
 /* eslint-disable sort-keys */
 
 // The available endpoints that will show in the dropdown. For the most part (with the exception of
@@ -26,14 +25,6 @@ export function createTesting (t: TFunction, firstOnly: boolean, withSort: boole
         POLYGON: 'wss://testnet.polygonavail.net/ws',
       }
     },
-    {
-      info: 'matic',
-      isDevelopment: false,
-      text: t('rpc.matic', 'Avail-Devnet', { ns: 'apps-config' }),
-      providers: {
-        POLYGON: 'wss://devnet-avail.polygon.technology/ws'
-      }
-    }
     /* {
       info: 'aleph',
       text: t('rpc.test.aleph', 'Aleph Zero Testnet', { ns: 'apps-config' }),
