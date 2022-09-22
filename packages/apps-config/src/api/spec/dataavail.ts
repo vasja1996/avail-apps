978e7966f196d9f94ad472f8cdc1a1a150d64a47
Kailas <kailaskr05@gmail.com>
2022-09-22 11:43:28 +0530

    fix urls

--- packages/apps-config/src/api/spec/dataavail.ts
+++ packages/apps-config/src/api/spec/dataavail.ts
@@ -1,6 +1,6 @@
 // Copyright 2017-2021 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
-import type  {H256}  from '@polkadot/types/interfaces';
+// import type  {H256}  from '@polkadot/types/interfaces';
 import type { OverrideBundleDefinition } from '@polkadot/types/types';
 
 // structs need to be in order
