afe241d0928f8921c716a176b2bd62755d360325
Kailas <kailaskr05@gmail.com>
2023-06-06 18:35:12 +0530

    comment proposal

--- packages/page-tech-comm/src/App.tsx
+++ packages/page-tech-comm/src/App.tsx
@@ -35,12 +35,12 @@ function TechCommApp ({ basePath, className, type }: Props): React.ReactElement<
       isRoot: true,
       name: 'overview',
       text: t<string>('Overview')
-    },
-    {
-      name: 'proposals',
-      text: t<string>('Proposals ({{count}})', { replace: { count: (proposalHashes && proposalHashes.length) || 0 } })
     }
-  ], [proposalHashes, t]);
+    // {
+    //   name: 'proposals',
+    //   text: t<string>('Proposals ({{count}})', { replace: { count: (proposalHashes && proposalHashes.length) || 0 } })
+    // }
+  ], [t]);
 
   return (
     <main className={className}>
