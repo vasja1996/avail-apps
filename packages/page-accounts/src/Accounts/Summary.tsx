e1159e4f1e0ca61c5099b88143107d033ada4060
Kailas <kailaskr05@gmail.com>
2023-06-28 18:28:49 +0530

    fix on accounts

--- packages/page-accounts/src/Accounts/Summary.tsx
+++ packages/page-accounts/src/Accounts/Summary.tsx
@@ -20,48 +20,51 @@ function Summary ({ balance, className }: Props) {
 
   return (
     <SummaryBox className={className}>
-      <CardSummary label={t<string>('total balance')}>
-        <FormatBalance
-          className={balance ? '' : '--tmp'}
-          value={balance?.total || 1}
-        />
-      </CardSummary>
-      <CardSummary
-        className='media--900'
-        label={t<string>('total transferrable')}
-      >
-        <FormatBalance
-          className={balance ? '' : '--tmp'}
-          value={balance?.transferrable || 1}
-        />
-      </CardSummary>
-      <CardSummary label={t<string>('total locked')}>
-        <FormatBalance
-          className={balance ? '' : '--tmp'}
-          value={balance?.locked || 1}
-        />
-      </CardSummary>
-      {balance?.bonded.gtn(0) &&
+      {balance && (
+        <>
+          <CardSummary label={t<string>('total balance')}>
+            <FormatBalance
+              className={balance ? '' : '--tmp'}
+              value={balance?.total || 1}
+            />
+          </CardSummary>
+          <CardSummary
+            className='media--900'
+            label={t<string>('total transferrable')}
+          >
+            <FormatBalance
+              className={balance ? '' : '--tmp'}
+              value={balance?.transferrable || 1}
+            />
+          </CardSummary>
+          <CardSummary label={t<string>('total locked')}>
+            <FormatBalance
+              className={balance ? '' : '--tmp'}
+              value={balance?.locked || 1}
+            />
+          </CardSummary>
+          {balance?.bonded.gtn(0) &&
         <CardSummary
           className='media--1100'
           label={t<string>('bonded')}
         >
           <FormatBalance value={balance.bonded} />
         </CardSummary>}
-      {balance?.redeemable.gtn(0) &&
+          {balance?.redeemable.gtn(0) &&
         <CardSummary
           className='media--1500'
           label={t<string>('redeemable')}
         >
           <FormatBalance value={balance.redeemable} />
         </CardSummary>}
-      {balance?.unbonding.gtn(0) &&
+          {balance?.unbonding.gtn(0) &&
         <CardSummary
           className='media--1300'
           label={t<string>('unbonding')}
         >
           <FormatBalance value={balance.unbonding} />
         </CardSummary>}
+        </>)}
     </SummaryBox>
   );
 }
